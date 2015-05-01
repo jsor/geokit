dad651341918993ab2e6360b6992f5fca127673d
Jan Sorgalla <jsorgalla@gmail.com>
2015-05-01 08:06:45 +0200

    Add tests for ellipsoids factories

--- tests/EllipsoidTest.php
+++ tests/EllipsoidTest.php
@@ -211,4 +211,74 @@ class EllipsoidTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('\InvalidArgumentException', 'Cannot normalize Ellipsoid from input {}.');
         Ellipsoid::normalize(new \stdClass());
     }
+
+    public function testWgs84Factory()
+    {
+        $ellipsoid = Ellipsoid::wgs84();
+
+        $this->assertSame(6378137.0, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356752.3142451793, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.0033528106647474805, $ellipsoid->getFlattening());
+        $this->assertSame(298.257223563, $ellipsoid->getInverseFlattening());
+    }
+
+    public function testWgs72Factory()
+    {
+        $ellipsoid = Ellipsoid::wgs72();
+
+        $this->assertSame(6378135.0, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356750.5200160937, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.003352779454167505, $ellipsoid->getFlattening());
+        $this->assertSame(298.26, $ellipsoid->getInverseFlattening());
+    }
+
+    public function testWgs66Factory()
+    {
+        $ellipsoid = Ellipsoid::wgs66();
+
+        $this->assertSame(6378145.0, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356759.7694886839, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.0033528918692372171, $ellipsoid->getFlattening());
+        $this->assertSame(298.25, $ellipsoid->getInverseFlattening());
+    }
+
+    public function testGrs80Factory()
+    {
+        $ellipsoid = Ellipsoid::grs80();
+
+        $this->assertSame(6378137.0, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356752.3141403561, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.0033528106811823188, $ellipsoid->getFlattening());
+        $this->assertSame(298.257222101, $ellipsoid->getInverseFlattening());
+    }
+
+    public function testAnsFactory()
+    {
+        $ellipsoid = Ellipsoid::ans();
+
+        $this->assertSame(6378160.0, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356774.7191953054, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.0033528918692372171, $ellipsoid->getFlattening());
+        $this->assertSame(298.25, $ellipsoid->getInverseFlattening());
+    }
+
+    public function testAiry1830Factory()
+    {
+        $ellipsoid = Ellipsoid::airy1830();
+
+        $this->assertSame(6377563.396, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356256.9092372851, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.0033408506414970775, $ellipsoid->getFlattening());
+        $this->assertSame(299.3249646, $ellipsoid->getInverseFlattening());
+    }
+
+    public function testKrassovsky1940Factory()
+    {
+        $ellipsoid = Ellipsoid::krassovsky1940();
+
+        $this->assertSame(6378245.0, $ellipsoid->getSemiMajorAxis());
+        $this->assertSame(6356863.0187730473, $ellipsoid->getSemiMinorAxis());
+        $this->assertSame(0.003352329869259135, $ellipsoid->getFlattening());
+        $this->assertSame(298.3, $ellipsoid->getInverseFlattening());
+    }
 }
