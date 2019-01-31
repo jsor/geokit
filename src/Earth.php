1eb4303c4c0cd0035a851e35751236880217fdf8
Jan Sorgalla <jsorgalla@gmail.com>
2019-01-31 16:45:32 +0100

    Hard-code FLATTENING and extend documentation

--- src/Earth.php
+++ src/Earth.php
@@ -4,19 +4,32 @@ declare(strict_types=1);
 
 namespace Geokit;
 
+/**
+ * @see http://en.wikipedia.org/wiki/World_Geodetic_System
+ * @see https://en.wikipedia.org/wiki/Earth_radius
+ */
 final class Earth
 {
-    /**
-     * Mean earth radius
-     * @see https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
-     */
-    public const RADIUS = 6371008.8;
-
     public const SEMI_MAJOR_AXIS = 6378137.0;
-
-    public const SEMI_MINOR_AXIS = 6356752.3142;
 
     public const INVERSE_FLATTENING = 298.257223563;
 
-    public const FLATTENING = 1 / self::INVERSE_FLATTENING;
+    /**
+     * SEMI_MAJOR_AXIS - SEMI_MAJOR_AXIS / INVERSE_FLATTENING
+     */
+    public const SEMI_MINOR_AXIS = 6356752.3142;
+
+    /**
+     * 1 / INVERSE_FLATTENING
+     */
+    public const FLATTENING = 0.0033528106647475;
+
+    /**
+     * Mean earth radius
+     *
+     * (2 * SEMI_MAJOR_AXIS + SEMI_MINOR_AXIS) / 3
+     *
+     * @see https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
+     */
+    public const RADIUS = 6371008.8;
 }
